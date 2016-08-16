fc9c8aec2285870e8af5105c0d04368e4a6c386e
JulieLev <jkgee1@yahoo.co.uk>
2016-08-16 23:47:17 +0100

    array length test fine but extended contains test not quite working

--- test/note-list-model-test.js
+++ test/note-list-model-test.js
@@ -1,12 +1,25 @@
 var assert = require("../lib/assert");
 var List = require("../src/note-list-model").List;
 
-function testCreateList() {
+function testNoteInList() {
   var list = new List();
   var array = list.getNotes();
+  list.addNote("Hello")
   list.addNote("My favourite language is Javascript");
+  list.addNote("Goodbye")
 
-  assert.arrayContains(array, "My favourite language is Javascript");
+  assert.arrayContains("My favourite language is Javascript", array);
 };
 
-testCreateList();
+function testListStoresMultipleNotes() {
+  var list = new List();
+  var array = list.getNotes();
+  list.addNote("Hello")
+  list.addNote("My favourite language is Javascript");
+  list.addNote("Goodbye")
+
+  assert.arrayLength(array, 3);
+};
+
+testListStoresMultipleNotes();
+testNoteInList();
