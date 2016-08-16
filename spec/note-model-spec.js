4b1b000daf2067c0efa4a5ed00e327c44a6f0e9a
JulieLev <jkgee1@yahoo.co.uk>
2016-08-16 17:37:47 +0100

    Challenge 1 complete

---
+++ spec/note-model-spec.js
@@ -0,0 +1,10 @@
+var assert = require("../lib/assert");
+var Note = require("../src/note-model").Note;
+
+function testCreateNote() {
+  var note = new Note("My favourite language is Javascript");
+  assert.isTrue(note.getText() === "My favourite language is Javascript");
+
+};
+
+testCreateNote();
