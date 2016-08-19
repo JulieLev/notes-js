54372358b46b16b182d6c7180ccb28d8d17975bb
JulieLev <jkgee1@yahoo.co.uk>
2016-08-19 10:28:27 +0100

    localhost removed from html string

--- spec/note-controller-tests.js
+++ spec/note-controller-tests.js
@@ -2,7 +2,7 @@ function testNoteController () {
   noteController = new NoteController("Hello, World!");
   noteController.insertHTML();
   var outputStr = document.getElementById("app").innerHTML;
-  if (outputStr === "<ul><li>Hello, World!</li></ul>"){
+  if (outputStr === "<ul><li><a href=\"\#0\">Hello, World!</a></li></ul>") {
     console.log("Pass: Note displayed in the html page");
   }
   else {
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -27,14 +27,13 @@
         stringNote = note.getText();
         noteId = note.getId();
         stringNote = stringNote.substring(0,20);
-        stringNote =
-        stringLimit20 +="<li><a href=\"localhost8080:\#" + noteId + "\">" + stringNote + "</a></li>";
+        stringLimit20 +="<li><a href=\"\#" + noteId + "\">" + stringNote + "</a></li>";
       });
         stringLimit20 = "<ul>" + stringLimit20 + "</ul>";
     }
       return stringLimit20;
   };
-// "<ul><li><a href=\"localhost8080:\#0\">I am Julie - ha ha</a></li></ul>"
+
 exports.NoteListView = NoteListView;
 
 })(this);
