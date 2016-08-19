22e896a7e75009cb7ea9bb12ac4f8aa96db03760
benjamin-white <benjaminwhite@live.co.uk>
2016-08-19 13:57:10 +0100

    updates to show single note

--- src/note-controller.js
+++ src/note-controller.js
@@ -3,12 +3,26 @@
   function NoteController(text){
     var list = new NoteList();
     list.addNote(text);
+    this.noteList = list.displayList();
     this.noteListView = new NoteListView(list.displayList());
-  }
+    window.addEventListener('hashchange', this.showNoteForCurrentId);
+  };
 
   NoteController.prototype.insertHTML = function() {
     var element = document.getElementById("app");
     element.innerHTML = this.noteListView.outputTwenty();
+  };
+
+  NoteController.prototype.showNoteForCurrentId = function() {
+    var noteId = location.hash.split("#")[1];
+    var singleNote;
+
+    noteController.noteList.forEach(function(item) {
+      if (item.id === Number(noteId)) {singleNote = item}
+    });
+
+    var singleNoteView = new SingleNoteView(singleNote);
+    document.getElementById('app').innerHTML = singleNoteView.writeHTML();
   };
 
   exports.NoteController = NoteController;
